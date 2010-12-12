4161039b5d2a9a934cdfc4b77851add05bfbb27c
tk0miya <i.tkomiya@gmail.com>
2010-12-12 14:13:52 +0900

    * Added copyright text.

--- tk0/gaerunner.py
+++ tk0/gaerunner.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright 2007 Google Inc.
+# Copyright 2010 tk0miya <i.tk0miya at gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
